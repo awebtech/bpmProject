4984697a4bf3018cdd196733af91bc8dc4681eac
awebtech <alexander.webtech@gmail.com>
2011-06-10 20:12:21 +0400

    Implementing Task-Create web-service

--- application/controllers/TaskController.class.php
+++ application/controllers/TaskController.class.php
@@ -832,18 +832,21 @@ class TaskController extends ApplicationController {
 	 * @return null
 	 */
 	function add_task() {
-		if (/*($_POST && 1) || */logged_user()->isGuest()) {
+		if (($_POST && 1) || logged_user()->isGuest()) {
 			
 			//$this->addHelper('data_mapping');			
 			//$os_id = $_POST['task']['object_subtype'];			
 			//$gid = get_group_by_object_subtype($os_id);			
-			//error_log(print_r($gid, true)."\n\n", 3, 'C:\FILES\SERVER\php535\error.log');
-			//error_log(print_r($_POST, true), 3, 'C:\FILES\SERVER\php535\error.log');			
+			//error_log(print_r($gid, true)."\n\n", 3, 'C:\FILES\SERVER\php535\error.log'););
+			error_log(print_r($_POST, true), 3, 'C:\FILES\SERVER\php535\error.log');
+			error_log(print_r($_GET, true), 3, 'C:\FILES\SERVER\php535\error.log');
 			flash_error(lang('no access permissions'));			
 			ajx_current("empty");
 			return;
 		}
+		
 		$project = active_or_personal_project();
+		
 		if(!ProjectTask::canAdd(logged_user(), $project)) {
 			flash_error(lang('no access permissions'));
 			ajx_current("empty");
@@ -851,10 +854,38 @@ class TaskController extends ApplicationController {
 		} // if
 
 		$task = new ProjectTask();
-		$task_data = array_var($_POST, 'task');		
+		$task_data = array_var($_POST, 'task');
 
 		if (is_array($task_data)) {
+			try {
+				$wso = new MilestoneWso($_POST);
+				$wso = $wso->getWsoState('Task');
+				
+				$client = new WebServiceClient('Construction_Service');
+				$result = $client->Start_Project_FO($wso); // Create new milestone via BPMS
+				
+				if (!empty($result->error)) {
+					throw new Exception($result->error);					
+				} else {
+					$milestone_id = $result->milestone_id;
+				}
+			} catch (Exception $e) {
+				flash_error($e->getMessage());
+				ajx_current("empty");
+				return;
+			}
 			
+			if ($task->getIsTemplate()) {
+				flash_success(lang('success add template', $task->getTitle()));
+			} else {
+				flash_success(lang('success add task list', $task->getTitle()));
+			}
+			if (array_var($task_data, 'inputtype') != 'taskview') {
+				ajx_current("back");
+			} else {
+				ajx_current("reload");
+			}
+			return;
 		} else {
 			$task_data = array(
 				'milestone_id' => array_var($_POST, 'milestone_id',0),
@@ -903,143 +934,147 @@ class TaskController extends ApplicationController {
 			ajx_current("empty");
 			return;
 		}
+		
 		$project = active_or_personal_project();
+		
 		if(!ProjectTask::canAdd(logged_user(), $project)) {
 			flash_error(lang('no access permissions'));
 			ajx_current("empty");
 			return;
 		} // if		
 		
-		$proj = Projects::findById(array_var($task_data, 'project_id'));
-		if ($proj instanceof Project) {
-			$project = $proj;
+		$task = new ProjectTask();
+		$task_data = array_var($_POST, 'task');
+		
+		tpl_assign('task_data', $task_data);
+		tpl_assign('task', $task);
+	
+		if (is_array($task_data)) {
+			$proj = Projects::findById(array_var($task_data, 'project_id'));
+			if ($proj instanceof Project) {
+				$project = $proj;
+			}
+			// order
+			$task->setOrder(ProjectTasks::maxOrder(array_var($task_data, "parent_id", 0), array_var($task_data, "milestone_id", 0)));
+
+			$task_data['due_date'] = getDateValue(array_var($_POST, 'task_due_date'));
+			$task_data['start_date'] = getDateValue(array_var($_POST, 'task_start_date'));
+			try {
+				$err_msg = $this->setRepeatOptions($task_data);
+				if ($err_msg) {
+					flash_error($err_msg);
+					ajx_current("empty");
+					return;
+				}
+
+				$task->setFromAttributes($task_data);
+
+				$totalMinutes = (array_var($task_data, 'time_estimate_hours',0) * 60) +
+						(array_var($task_data, 'time_estimate_minutes',0));
+				$task->setTimeEstimate($totalMinutes);
+
+				$task->setIsPrivate(false); // Not used, but defined as not null.
+				// Set assigned to
+				$assigned_to = explode(':', array_var($task_data, 'assigned_to', ''));
+				$company_id = array_var($assigned_to, 0, 0);
+				$user_id = array_var($assigned_to, 1, 0);
+				$can_assign = can_assign_task_to_company_user(logged_user(), $task, $company_id, $user_id);
+				if ($can_assign !== true) {
+					flash_error($can_assign);
+					ajx_current("empty");
+					return;
+				}
+				$task->setAssignedToCompanyId($company_id);
+				$task->setAssignedToUserId($user_id);
+
+				/* Commented by awebtech, there is no GET during "Task->Create" web-service operation processing
+				$id = array_var($_GET, 'id', 0);
+				$parent = ProjectTasks::findById($id);
+				if ($parent instanceof ProjectTask) {
+					$task->setParentId($id);
+					if ($parent->getIsTemplate()) {
+						$task->setIsTemplate(true);
+					}
+				}
+				*/
+
+				if ($task->getParentId() > 0 && $task->hasChild($task->getParentId())) {
+					flash_error(lang('task child of child error'));
+					ajx_current("empty");
+					return;
+				}
+
+				//Add handins
+				$handins = array();
+				for($i = 0; $i < 4; $i++) {
+					if(isset($task_data["handin$i"]) && is_array($task_data["handin$i"]) && (trim(array_var($task_data["handin$i"], 'title')) <> '')) {
+						$assigned_to = explode(':', array_var($task_data["handin$i"], 'assigned_to', ''));
+						$handins[] = array(
+							'title' => array_var($task_data["handin$i"], 'title'),
+							'responsible_company_id' => array_var($assigned_to, 0, 0),
+							'responsible_user_id' => array_var($assigned_to, 1, 0)
+						); // array
+					} // if
+				} // for
+
+
+				DB::beginWork();
+				$task->save();
+				//$task->setProject($project);
+				//echo 'pepe'; DB::rollback(); die();
+				$task->setTagsFromCSV(array_var($task_data, 'tags'));
+
+				foreach($handins as $handin_data) {
+					$handin = new ObjectHandin();
+					$handin->setFromAttributes($handin_data);
+					$handin->setObjectId($task->getId());
+					$handin->setObjectManager(get_class($task->manager()));
+					$handin->save();
+				} // foreach*/
+
+				/* Commented by awebtech, there is no GET during "Task->Create" web-service operation processing
+				if (array_var($_GET, 'copyId', 0) > 0) {
+					// copy remaining stuff from the task with id copyId
+					$toCopy = ProjectTasks::findById(array_var($_GET, 'copyId'));
+					if ($toCopy instanceof ProjectTask) {
+						ProjectTasks::copySubTasks($toCopy, $task, array_var($task_data, 'is_template', false));
+					}
+				}
+				*/
+
+				//Link objects
+				$object_controller = new ObjectController();
+				if ($parent instanceof ProjectTask) {
+					// task is being added as subtask of another, so place in same workspace
+					$task->addToWorkspace($parent->getProject());
+				} else {
+					$object_controller->add_to_workspaces($task);
+				}
+				$object_controller->link_to_new_object($task);
+				$object_controller->add_subscribers($task);
+				$object_controller->add_custom_properties($task);
+				$object_controller->add_reminders($task);
+
+				ApplicationLogs::createLog($task, $task->getWorkspaces(), ApplicationLogs::ACTION_ADD);
+
+				DB::commit();
+
+				// notify asignee
+				if(array_var($task_data, 'send_notification') == 'checked') {
+					try {
+						Notifier::taskAssigned($task);
+					} catch(Exception $e) {
+						evt_add("debug", $e->getMessage());
+					} // try
+				}
+				
+				self::$mainObjectId = $task->getId();
+			} catch(Exception $e) {
+				DB::rollback();
+				flash_error($e->getMessage());
+				ajx_current("empty");
+			} // try
 		}
-		// order
-		$task->setOrder(ProjectTasks::maxOrder(array_var($task_data, "parent_id", 0), array_var($task_data, "milestone_id", 0)));
-
-		$task_data['due_date'] = getDateValue(array_var($_POST, 'task_due_date'));
-		$task_data['start_date'] = getDateValue(array_var($_POST, 'task_start_date'));
-		try {
-			$err_msg = $this->setRepeatOptions($task_data);
-			if ($err_msg) {
-				flash_error($err_msg);
-				ajx_current("empty");
-				return;
-			}
-
-			$task->setFromAttributes($task_data);
-
-			$totalMinutes = (array_var($task_data, 'time_estimate_hours',0) * 60) +
-					(array_var($task_data, 'time_estimate_minutes',0));
-			$task->setTimeEstimate($totalMinutes);
-
-			$task->setIsPrivate(false); // Not used, but defined as not null.
-			// Set assigned to
-			$assigned_to = explode(':', array_var($task_data, 'assigned_to', ''));
-			$company_id = array_var($assigned_to, 0, 0);
-			$user_id = array_var($assigned_to, 1, 0);
-			$can_assign = can_assign_task_to_company_user(logged_user(), $task, $company_id, $user_id);
-			if ($can_assign !== true) {
-				flash_error($can_assign);
-				ajx_current("empty");
-				return;
-			}
-			$task->setAssignedToCompanyId($company_id);
-			$task->setAssignedToUserId($user_id);
-
-			$id = array_var($_GET, 'id', 0);
-			$parent = ProjectTasks::findById($id);
-			if ($parent instanceof ProjectTask) {
-				$task->setParentId($id);
-				if ($parent->getIsTemplate()) {
-					$task->setIsTemplate(true);
-				}
-			}
-
-			if ($task->getParentId() > 0 && $task->hasChild($task->getParentId())) {
-				flash_error(lang('task child of child error'));
-				ajx_current("empty");
-				return;
-			}
-
-			//Add handins
-			$handins = array();
-			for($i = 0; $i < 4; $i++) {
-				if(isset($task_data["handin$i"]) && is_array($task_data["handin$i"]) && (trim(array_var($task_data["handin$i"], 'title')) <> '')) {
-					$assigned_to = explode(':', array_var($task_data["handin$i"], 'assigned_to', ''));
-					$handins[] = array(
-						'title' => array_var($task_data["handin$i"], 'title'),
-						'responsible_company_id' => array_var($assigned_to, 0, 0),
-						'responsible_user_id' => array_var($assigned_to, 1, 0)
-					); // array
-				} // if
-			} // for
-
-
-			DB::beginWork();
-			$task->save();
-			//$task->setProject($project);
-			//echo 'pepe'; DB::rollback(); die();
-			$task->setTagsFromCSV(array_var($task_data, 'tags'));
-
-			foreach($handins as $handin_data) {
-				$handin = new ObjectHandin();
-				$handin->setFromAttributes($handin_data);
-				$handin->setObjectId($task->getId());
-				$handin->setObjectManager(get_class($task->manager()));
-				$handin->save();
-			} // foreach*/
-
-			if (array_var($_GET, 'copyId', 0) > 0) {
-				// copy remaining stuff from the task with id copyId
-				$toCopy = ProjectTasks::findById(array_var($_GET, 'copyId'));
-				if ($toCopy instanceof ProjectTask) {
-					ProjectTasks::copySubTasks($toCopy, $task, array_var($task_data, 'is_template', false));
-				}
-			}
-
-			//Link objects
-			$object_controller = new ObjectController();
-			if ($parent instanceof ProjectTask) {
-				// task is being added as subtask of another, so place in same workspace
-				$task->addToWorkspace($parent->getProject());
-			} else {
-				$object_controller->add_to_workspaces($task);
-			}
-			$object_controller->link_to_new_object($task);
-			$object_controller->add_subscribers($task);
-			$object_controller->add_custom_properties($task);
-			$object_controller->add_reminders($task);
-
-			ApplicationLogs::createLog($task, $task->getWorkspaces(), ApplicationLogs::ACTION_ADD);
-
-			DB::commit();
-
-			// notify asignee
-			if(array_var($task_data, 'send_notification') == 'checked') {
-				try {
-					Notifier::taskAssigned($task);
-				} catch(Exception $e) {
-					evt_add("debug", $e->getMessage());
-				} // try
-			}
-
-			if ($task->getIsTemplate()) {
-				flash_success(lang('success add template', $task->getTitle()));
-			} else {
-				flash_success(lang('success add task list', $task->getTitle()));
-			}
-			if (array_var($task_data, 'inputtype') != 'taskview') {
-				ajx_current("back");
-			} else {
-				ajx_current("reload");
-			}
-			self::$mainObjectId = $task->getId();
-		} catch(Exception $e) {
-			DB::rollback();
-			flash_error($e->getMessage());
-			ajx_current("empty");
-		} // try
 	}
 	
 	/**
@@ -1103,10 +1138,10 @@ class TaskController extends ApplicationController {
 	 * @param void
 	 * @return null
 	 */
-	function edit_task() {
-		//error_log(print_r($_POST, true));
-		//error_log(print_r($_GET, true));
-		if (logged_user()->isGuest()) {
+	function edit_task() {		
+		if (/*($_POST && 1) && */logged_user()->isGuest()) {
+			//error_log(print_r($_POST, true));
+			//error_log(print_r($_GET, true));
 			flash_error(lang('no access permissions'));
 			ajx_current("empty");
 			return;
